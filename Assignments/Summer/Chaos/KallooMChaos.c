445a982178b7163546a086e47d9e7345e42d9e99
Mitra Kalloo <Mkalloo42@gmail.com>
2016-06-29 10:15:03 -0400

    finish assignment

--- Assignments/Summer/Chaos/KallooMChaos.c
+++ Assignments/Summer/Chaos/KallooMChaos.c
@@ -1,56 +1,105 @@
-// Program to print all combination of size r in an array of size n
+/*
+ * main.c
+ *
+ *  Created on: Jun 29, 2016
+ *      Author: Mitra
+ */
+
 #include<stdio.h>
-void combinationUtil(FILE* out,int arr[],int n,int r,int index,int data[],int i);
- 
-// The main function that prints all combinations of size r
-// in arr[] of size n. This function mainly uses combinationUtil()
-void printCombination(FILE* out,int arr[], int n, int r)
-{
-    // A temporary array to store all combination one by one
-    int data[r];
- 
-    // Print all combination using temprary array 'data[]'
-    combinationUtil(out,arr, n, r, 0, data, 0);
+#include <stdlib.h>
+#include <string.h>
+
+#define Word_Size 10
+
+void ReadInData();
+void combinationUtil(int n,int r,int index,int data[],int i);
+void printCombination(int n, int r);
+
+
+char** WORDS;
+char** ReverseWords;
+int CHAOS = 0;
+
+int main(){
+    FILE * out = fopen("output.txt","w");
+    FILE* in = fopen("input.txt","r");
+    
+    int n, r = 2;;
+    fscanf(in,"%d",&n);
+    fclose(in);
+
+
+
+   int a;
+
+   WORDS = (char**)malloc((n+1) * sizeof(char*));
+   			for( a = 0; a <= n; a++){
+   				WORDS[a] = (char*)malloc((Word_Size+1)*sizeof(char*));
+			   }
+   ReverseWords  = (char**)malloc((n+1) * sizeof(char*));
+		for( a = 0; a <= n; a++){
+			ReverseWords[a] = (char*)malloc((Word_Size+1)*sizeof(char*));
+	   }
+
+    ReadInData();
+    printCombination(n, r);
+    
+    fprintf(out,"%d\n",CHAOS);
+
+	
+	fclose(out);
+ return 0;
 }
- 
-/* arr[]  ---> Input Array
-   n      ---> Size of input array
-   r      ---> Size of a combination to be printed
-   index  ---> Current index in data[]
-   data[] ---> Temporary array to store current combination
-   i      ---> index of current element in arr[]     */
-void combinationUtil(FILE* out, int arr[], int n, int r, int index, int data[], int i)
+
+
+void ReadInData(){
+	int amt_words,i;
+	FILE*in = fopen("input.txt","r");
+	fscanf(in,"%d",&amt_words);
+
+	char temp_word[Word_Size+1];
+	for(i=1;i <= amt_words;i++){
+		fscanf(in,"%s",temp_word);
+		strcpy(WORDS[i],temp_word);
+		strcpy(ReverseWords[i],strrev(temp_word));
+	}
+	fclose(in);
+
+}
+
+
+void printCombination(int n, int r)
 {
-    // Current cobination is ready, print it
-    if (index == r)
-    {
-        int j;
-        for (j=0; j<r; j++)
-            fprintf(out,"%d ",data[j]);
-        fprintf(out,"\n");
+    int data[r];
+    combinationUtil(n, r, 0, data, 1);
+}
+
+
+void combinationUtil(int n, int r, int index, int data[], int i){
+    if (index == r){
+    	
+    	if (strcmp(WORDS[data[0]], WORDS[data[1]]) < 0){
+    		if (strcmp(ReverseWords[data[1]], ReverseWords[data[0]]) < 0){
+    			CHAOS++;
+			}
+		}
+    	
+    	if (strcmp(WORDS[data[1]], WORDS[data[0]]) < 0){
+    		if (strcmp(ReverseWords[data[0]], ReverseWords[data[1]]) < 0){
+    			CHAOS++;
+			}
+		}
         return;
     }
- 
-    // When no more elements are there to put in data[]
-    if (i >= n)
+
+    if (i > n)
         return;
- 
-    // current is included, put next at next location
-    data[index] = arr[i];
-    combinationUtil(out,arr, n, r, index+1, data, i+1);
- 
-    // current is excluded, replace it with next (Note that
-    // i+1 is passed, but index is not changed)
-    combinationUtil(out,arr, n, r, index, data, i+1);
+
+ 	data[index] = i;
+    combinationUtil(n, r, index+1, data, i+1);
+
+    combinationUtil(n, r, index, data, i+1);
 }
- 
-// Driver program to test above functions
-int main()
-{
-    FILE * out = fopen("output.txt","w");
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60};
-    int r = 2;
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printCombination(out,arr, n, r);
-    return 0;
-}
+
+
+
